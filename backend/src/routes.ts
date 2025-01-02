0a84413a179ca0b20d3d5a26256991e794c83053
gabrielglasser <sga91000@gmail.com>
2025-01-02 20:56:29 -0300

    Tratando erros

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,6 +4,7 @@ const router = Router();
 
 router.get("/teste", (req: Request, res: Response) => {
     res.send("Hello World!");
+    throw new Error("Erro ao fazer a requisição");
 });
 
 export { router };
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,30 @@
 import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors";
+import cors from "cors";
+
+
 import { router } from "./routes";
 
 const app = express();
-
 app.use(express.json());
+app.use(cors());
 
 app.use(router);
 
-app.listen(3031, () => {console.log("Server is running on port 3031")});
-git
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    //se for uma instancia do tipo Error
+    return res.status(400).json({
+      error: err.message,
+    });
+  }
+
+  return res.status(500).json({
+      status: "error",
+      message: "Internal server error"
+  })
+});
+
+app.listen(3031, () => {
+  console.log("Server is running on port 3031");
+});
