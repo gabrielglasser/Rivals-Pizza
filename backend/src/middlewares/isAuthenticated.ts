0096ff3065a95de8f17be57b9318442aa02982df
gabrielglasser <sga91000@gmail.com>
2025-01-04 20:16:34 -0300

    Criando middleware

--- backend/src/middlewares/isAuthenticated.ts
+++ backend/src/middlewares/isAuthenticated.ts
@@ -1,7 +1,33 @@
 import { Request, Response, NextFunction } from "express";
+import { verify } from "jsonwebtoken";
+
+interface Payload {
+  sub: string;
+}
 
 export function isAuthenticated(
   req: Request,
   res: Response,
   next: NextFunction
-) {}
+) {
+  //receber o token
+  const authToken = req.headers.authorization;
+
+  if (!authToken) {
+    return res.status(401).end();
+  }
+
+  const [, token] = authToken.split(" ");
+
+  try {
+    //validar esse token
+    const { sub } = verify(token, process.env.JWT_SECRET) as Payload;
+
+    //recuperar o id do usuario
+    //req.user_id = sub;
+
+    return next();
+  } catch (err) {
+    return res.status(401).end();
+  }
+}
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,6 +4,8 @@ import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
 
+import { isAuthenticated } from "./middlewares/isAuthenticated";
+
 const router = Router();
 
 //ROTAS USER
@@ -15,7 +17,7 @@ router.post("/session", (req, res, next) => {
     new AuthUserController().handle(req, res);
   });
 
-  router.get("/me", (req, res, next) => {
+  router.get("/me", isAuthenticated, (req, res, next) => { 
     new DetailUserController().handle(req, res);
   });
 
