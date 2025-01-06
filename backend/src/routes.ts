bd480664b25baca7975041fd661c2cff042ee413
gabrielglasser <sga91000@gmail.com>
2025-01-06 12:27:24 -0300

    Cadastrando categorias

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,21 +4,30 @@ import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
 
+import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
+
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 
 const router = Router();
 
 //ROTAS USER
 router.post("/users", (req, res, next) => {
-    new CreateUserController().handle(req, res);
-  });
+  new CreateUserController().handle(req, res);
+});
 
 router.post("/session", (req, res, next) => {
-    new AuthUserController().handle(req, res);
-  });
+  new AuthUserController().handle(req, res);
+});
 
-  router.get("/me", isAuthenticated, (req, res, next) => { 
-    new DetailUserController().handle(req, res);
-  });
+router.get("/me", (req, res, next) => {
+  isAuthenticated;
+  new DetailUserController().handle(req, res);
+});
+
+//ROTAS CATEGORY
+router.post("/category", (req, res, next) => {
+  isAuthenticated;
+  new CreateCategoryController().handle(req, res);
+});
 
 export { router };
