59803aa803252f9ab7fc9407c511e51978c9b39a
gabrielglasser <sga91000@gmail.com>
2025-01-14 21:44:09 -0300

    Criando pagina login

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,36 @@
 import styles from "./page.module.scss";
+import logoimg from "/public/logo.png";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Page() {
   return (
-    <main>
-      <h1 className={styles.teste}> Teste</h1>
-    </main>
+    <>
+      <div className={styles.containerCenter}>
+        <Image src={logoimg} alt="logo" />
+
+        <section className={styles.login}>
+          <form>
+            <input
+              type="email"
+              required
+              name="email"
+              placeholder="Digite seu email"
+              className={styles.input}
+            />
+            <input
+              type="password"
+              required
+              name="password"
+              placeholder="Digite sua senha"
+              className={styles.input}
+            />
+            <button type="submit">Acessar</button>
+          </form>
+            <Link href="/singup" className={styles.text}>Não possui uma conta? Cadastre-se</Link>
+
+        </section>
+      </div>
+    </>
   );
 }
