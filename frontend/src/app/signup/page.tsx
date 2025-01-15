d7c07bdf37df57bf3b2160eedc6fe448204c75cb
gabrielglasser <sga91000@gmail.com>
2025-01-15 18:29:48 -0300

    Cadastrando usuarios

--- frontend/src/app/signup/page.tsx
+++ frontend/src/app/signup/page.tsx
@@ -2,17 +2,44 @@ import Image from "next/image";
 import Link from "next/link";
 import styles from "../page.module.scss";
 import logoimg from "/public/logo.png";
-
+import { api } from "@/services/api";
+import { redirect } from "next/navigation";
 
 export default function Signup() {
+  async function handleRegister(formData: FormData) {
+    "use server";
+
+    const name = formData.get("name") as string;
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+
+    if (name === "" || email === "" || password === "") {
+      throw new Error("Preencha todos os campos");
+      return;
+    }
+
+    try {
+      await api.post("/users", {
+        name,
+        email,
+        password,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+
+    redirect("/");
+
+  }
+
   return (
     <>
       <div className={styles.containerCenter}>
         <Image src={logoimg} alt="logo" />
 
         <section className={styles.login}>
-            <h1>Criando sua conta</h1>
-          <form>
+          <h1>Criando sua conta</h1>
+          <form action={handleRegister}>
             <input
               type="text"
               required
@@ -34,10 +61,13 @@ export default function Signup() {
               placeholder="Digite sua senha"
               className={styles.input}
             />
-            <button type="submit" className={styles.button}>Cadastrar</button>
+            <button type="submit" className={styles.button}>
+              Cadastrar
+            </button>
           </form>
-            <Link href="/" className={styles.text}>Já possui uma conta? Acesse</Link>
-
+          <Link href="/" className={styles.text}>
+            Já possui uma conta? Acesse
+          </Link>
         </section>
       </div>
     </>
