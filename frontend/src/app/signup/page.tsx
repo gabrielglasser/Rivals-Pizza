9a7dbf7aeff03f4cfb2de7a89b97ceeca06e2b6b
gabrielglasser <sga91000@gmail.com>
2025-01-15 17:55:03 -0300

    Criando pagina signup

--- frontend/src/app/signup/page.tsx
+++ frontend/src/app/signup/page.tsx
@@ -1,9 +1,45 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "../page.module.scss";
+import logoimg from "/public/logo.png";
+
+
 export default function Signup() {
   return (
     <>
-      <main>
-        <h1>Signup</h1>
-      </main>
+      <div className={styles.containerCenter}>
+        <Image src={logoimg} alt="logo" />
+
+        <section className={styles.login}>
+            <h1>Criando sua conta</h1>
+          <form>
+            <input
+              type="text"
+              required
+              name="name"
+              placeholder="Digite seu nome"
+              className={styles.input}
+            />
+            <input
+              type="email"
+              required
+              name="email"
+              placeholder="Digite seu email"
+              className={styles.input}
+            />
+            <input
+              type="password"
+              required
+              name="password"
+              placeholder="Digite sua senha"
+              className={styles.input}
+            />
+            <button type="submit" className={styles.button}>Cadastrar</button>
+          </form>
+            <Link href="/" className={styles.text}>Já possui uma conta? Acesse</Link>
+
+        </section>
+      </div>
     </>
   );
 }
