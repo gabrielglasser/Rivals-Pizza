130aa62dd5b33b12705905b3fabe6fbfa1316716
gabrielglasser <sga91000@gmail.com>
2025-01-21 18:24:22 -0300

    Cadastrando categoria

--- frontend/src/app/dashboard/category/page.tsx
+++ frontend/src/app/dashboard/category/page.tsx
@@ -1,22 +1,42 @@
 import styles from "./styles.module.scss";
 import { Button } from "../components/button";
 import { api } from "@/services/api";
+import { redirect } from "next/navigation";
+
+import { getCookieServer } from "@/lib/cookieServer";
 
 export default function Category() {
+  async function handleRegisterCategory(formData: FormData) {
+    "use server";
 
-    async function handleRegisterCategory(formData: FormData) {
-      "use server";
-  
-     
+    const name = formData.get("name") as string;
+
+    if (name === "") {
+      return;
     }
 
+    const data = {
+      name: name,
+    };
+
+    const token = await getCookieServer();
+
+    await api.post("/category", data, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      }
+    }).catch((err) => {
+      console.log(err) 
+      return});
+
+    redirect("/dashboard");
+  }
 
   return (
     <main className={styles.container}>
       <h1>Nova Categoria</h1>
 
-      <form className={styles.form}
-      action={handleRegisterCategory}>
+      <form className={styles.form} action={handleRegisterCategory}>
         <input
           type="text"
           name="name"
@@ -26,7 +46,6 @@ export default function Category() {
         />
 
         <Button name="Cadastrar" />
-        
       </form>
     </main>
   );
