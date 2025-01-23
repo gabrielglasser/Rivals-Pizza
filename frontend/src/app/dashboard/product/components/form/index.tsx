660f96cc2b1c1d07a6b364612721d9dc840c55c6
gabrielglasser <sga91000@gmail.com>
2025-01-23 13:53:34 -0300

    Preview prodouto

--- frontend/src/app/dashboard/product/components/form/index.tsx
+++ frontend/src/app/dashboard/product/components/form/index.tsx
@@ -3,25 +3,29 @@
 import { ChangeEvent, useState } from "react";
 import styles from "./styles.module.scss";
 import { UploadCloud } from "lucide-react";
-import Image  from "next/image";
+import Image from "next/image";
+import { Button } from "@/app/dashboard/components/button";
 
 export function Form() {
+  const [image, setImage] = useState<File>();
+  const [previewImage, setPreviewImage] = useState<string>();
 
-    const [image, setImage] = useState<File>();
-    const [previewImage, setPreviewImage] = useState<string>();
+  function handleFile(e: ChangeEvent<HTMLInputElement>) {
+    if (e.target.files && e.target.files[0]) {
+      const image = e.target.files[0];
 
-    function handleFile(e: ChangeEvent<HTMLInputElement>) {
-        if (e.target.files && e.target.files[0]) {
-          const image = e.target.files[0];
-
-          if (image.type !== "image/png" && image.type !== "image/jpeg" && image.type !== ("image/jpg")) {
-            return;
-          }
-
-          setImage(image);
-          setPreviewImage(URL.createObjectURL(image));
-        }
+      if (
+        image.type !== "image/png" &&
+        image.type !== "image/jpeg" &&
+        image.type !== "image/jpg"
+      ) {
+        return;
       }
+
+      setImage(image);
+      setPreviewImage(URL.createObjectURL(image));
+    }
+  }
 
   return (
     <main className={styles.container}>
@@ -50,6 +54,38 @@ export function Form() {
             />
           )}
         </label>
+
+        <select name="category">
+          <option key={1} value={1}>
+            Pizzas
+          </option>
+          <option key={1} value={1}>
+            Massas
+          </option>
+        </select>
+        <input
+          type="text"
+          name="name"
+          placeholder="Digite o nome do produto"
+          required
+          className={styles.input}
+        />
+        <input
+          type="text"
+          name="price"
+          placeholder="Preço do produto"
+          required
+          className={styles.input}
+        />
+
+        <textarea
+          name="description"
+          placeholder="Digite a descrição do produto"
+          required
+          className={styles.input}
+        />
+
+        <Button name="Cadastrar produto"/>
       </form>
     </main>
   );
