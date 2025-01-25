c4357bd0dc1ca5985fc4d0386ba3297e4b90ec5b
gabrielglasser <sga91000@gmail.com>
2025-01-25 16:56:59 -0300

    Funcionalidade do modal

--- frontend/src/app/dashboard/components/orders/index.tsx
+++ frontend/src/app/dashboard/components/orders/index.tsx
@@ -1,33 +1,38 @@
+"use client";
 import styles from "./styles.module.scss";
 import { RefreshCw } from "lucide-react";
 import { OrderProps } from "@/lib/order.type";
 import { ModalOrder } from "../modal";
+import { use } from "react";
+import { OrderContext } from "@/providers/order";
 
 interface OrdersProps {
   orders: OrderProps[];
 }
 
 export function Orders({ orders }: OrdersProps) {
+  const { isOpen, onRequestOpen, onRequestClose } = use(OrderContext);
+
   return (
     <>
-    <main className={styles.container}>
-      <section className={styles.containerHeader}>
-        <h1>Últimos pedidos</h1>
-        <button>
-          <RefreshCw size={24} color="#3fffa3" />
-        </button>
-      </section>
-      <section className={styles.listOrders}>
-        {orders.map((order) => (
-          <button className={styles.orderItem} key={order.id}>
-            <div className={styles.tag}></div>
-            <span>Mesa {order.table}</span>
+      <main className={styles.container}>
+        <section className={styles.containerHeader}>
+          <h1>Últimos pedidos</h1>
+          <button>
+            <RefreshCw size={24} color="#3fffa3" />
           </button>
-        ))}
-      </section>
-    </main>
+        </section>
+        <section className={styles.listOrders}>
+          {orders.map((order) => (
+            <button className={styles.orderItem} key={order.id}>
+              <div className={styles.tag}></div>
+              <span>Mesa {order.table}</span>
+            </button>
+          ))}
+        </section>
+      </main>
 
-    <ModalOrder />
+      {isOpen && <ModalOrder />}
     </>
   );
 }
