998ac33f1064485ca4b575734aa9e891148e8112
gabrielglasser <sga91000@gmail.com>
2025-01-26 22:33:43 -0300

    Ajustando dashboard

--- frontend/src/app/dashboard/components/orders/index.tsx
+++ frontend/src/app/dashboard/components/orders/index.tsx
@@ -5,6 +5,8 @@ import { OrderProps } from "@/lib/order.type";
 import { ModalOrder } from "../modal";
 import { use } from "react";
 import { OrderContext } from "@/providers/order";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 interface OrdersProps {
   orders: OrderProps[];
@@ -12,9 +14,15 @@ interface OrdersProps {
 
 export function Orders({ orders }: OrdersProps) {
   const { isOpen, onRequestOpen, onRequestClose } = use(OrderContext);
+  const router = useRouter();
 
   async function handleDatailOrder(order_id: string) {
      await onRequestOpen(order_id);
+  }
+
+  function handleRefresh() {
+    router.refresh();
+    toast.success("Pedidos atualizados com sucesso!");
   }
 
   return (
@@ -22,11 +30,17 @@ export function Orders({ orders }: OrdersProps) {
       <main className={styles.container}>
         <section className={styles.containerHeader}>
           <h1>Últimos pedidos</h1>
-          <button>
+          <button onClick={handleRefresh}>
             <RefreshCw size={24} color="#3fffa3" />
           </button>
         </section>
         <section className={styles.listOrders}>
+          {orders.length === 0 && (
+            <span className={styles.emptyList}>
+              Nenhum pedido aberto foi encontrado...
+            </span>
+          )}
+
           {orders.map((order) => (
             <button className={styles.orderItem} key={order.id} onClick={ () => handleDatailOrder(order.id)}>
               <div className={styles.tag}></div>
